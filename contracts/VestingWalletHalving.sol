ddcc6910e0856bc7dcb448bf9b0936ce1a6efe0a
trizin <25263018+trizin@users.noreply.github.com>
2022-11-03 12:48:45 +0000

    Update OP version

--- contracts/VestingWalletHalving.sol
+++ contracts/VestingWalletHalving.sol
@@ -2,9 +2,9 @@
 // OpenZeppelin Contracts (last updated v4.7.0) (finance/VestingWallet.sol)
 pragma solidity ^0.8.0;
 
-import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/token/ERC20/utils/SafeERC20.sol";
-import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/utils/Address.sol";
-import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/utils/Context.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.7.0/contracts/token/ERC20/utils/SafeERC20.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.7.0/contracts/utils/Address.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.7.0/contracts/utils/Context.sol";
 
 /**
  * @title VestingWalletHalving
